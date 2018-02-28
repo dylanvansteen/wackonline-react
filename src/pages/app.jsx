cd7550e1272414c4721b4a391329620e7fb3754e
Dylan van Steen <dylanvansteen@gmail.com>
2018-02-28 09:27:40 +0100

    init

---
+++ src/pages/app.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Main = () => {
+    return <h1>mian</h1>;
+};
+
+export default Main;
---
+++ src/pages/login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from 'material-ui/Button';
+
+const Login = () => {
+    return (
+        <div>
+            <Button />
+
+            <h1>Login Page</h1>
+        </div>
+    );
+};
+
+export default Login;
